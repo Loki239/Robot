5f0ec863b7c6d6d8af3495711a0a9568b7974f7d
alex <aoklyunin@gmail.com>
2022-09-29 13:25:10 +0300

    works

--- src/main.cpp
+++ src/main.cpp
@@ -4,61 +4,16 @@
 
 #include <imgui-SFML.h>
 #include <imgui.h>
-#include <fstream>
-
-// первое множество
-static const int SET_1 = 0;
-// второе множество
-static const int SET_2 = 1;
-// пересечение множеств
-static const int SET_CROSSED = 2;
-// разность множеств
-static const int SET_SINGLE = 3;
 
 // Ширина окна
 static const int WINDOW_SIZE_X = 800;
 // Высота окна
 static const int WINDOW_SIZE_Y = 800;
 
-// путь к файлу вывода
-static const char OUTPUT_PATH[255] = "D:/Programming/Files/out.txt";
-// путь к файлу ввода
-static const char INPUT_PATH[255] = "D:/Programming/Files/in.txt";
-
-// Точка
-struct Point {
-    // положение
-    sf::Vector2<int> pos;
-    // номер множества
-    int setNum;
-
-    // конструктор
-    Point(const sf::Vector2<int> &pos, int setNum) : pos(pos), setNum(setNum) {
-    }
-
-    // получить случайную точку
-    static Point randomPoint() {
-        return Point(sf::Vector2<int>(
-                             rand() % WINDOW_SIZE_X,
-                             rand() % WINDOW_SIZE_Y),
-                     rand() % 2
-        );
-    }
-};
-
-// динамический список точек
-std::vector<Point> points;
-
 // цвет фона
 static sf::Color bgColor;
 // значение цвета по умолчанию
 float color[3] = {0.12f, 0.12f, 0.13f};
-
-// буфер, хранящий координаты последней добавленной вершины
-int lastAddPosBuf[2] = {0, 0};
-
-// буфер кол-ва случайных точек
-int lastRandoCntBuf[1] = {10};
 
 
 // задать цвет фона по вещественному массиву компонент
@@ -66,78 +21,6 @@ static void setColor(float *pDouble) {
     bgColor.r = static_cast<sf::Uint8>(pDouble[0] * 255.f);
     bgColor.g = static_cast<sf::Uint8>(pDouble[1] * 255.f);
     bgColor.b = static_cast<sf::Uint8>(pDouble[2] * 255.f);
-}
-
-
-// добавить заданное кол-во случайных точек
-void randomize(int cnt) {
-    for (int i = 0; i < cnt; i++) {
-        points.emplace_back(Point::randomPoint());
-    }
-}
-
-// запись в файл
-void saveToFile() {
-    // открываем поток данных для записи в файл
-    std::ofstream output(OUTPUT_PATH);
-
-    // перебираем точки
-    for (auto point: points) {
-        // выводим через пробел построчно: x-координату, y-координату и номер множества
-        output << point.pos.x << " " << point.pos.y << " " << point.setNum << std::endl;
-    }
-
-    // закрываем
-    output.close();
-}
-
-// решение задачи
-void solve() {
-    // у совпадающих по координатам точек меняем множество на SET_CROSSED
-    for (int i = 0; i < points.size(); i++)
-        for (int j = i + 1; j < points.size(); j++)
-            if (points[i].pos == points[j].pos)
-                points[i].setNum = points[j].setNum = SET_CROSSED;
-
-    // у всех точек, у которых множество не SET_CROSSED, задаём множество SET_SINGLE
-    for (auto &point: points)
-        if (point.setNum != SET_CROSSED)
-            point.setNum = SET_SINGLE;
-
-}
-
-// загрузка из файла
-void loadFromFile() {
-    // открываем поток данных для чтения из файла
-    std::ifstream input(INPUT_PATH);
-    // очищаем массив точек
-    points.clear();
-    // пока не достигнут конец файла
-    while (!input.eof()) {
-        int x, y, s;
-        input >> x; // читаем x координату
-        input >> y; // читаем y координату
-        input >> s; // читаем номер множества
-        // добавляем в динамический массив точку на основе прочитанных данных
-        points.emplace_back(Point(sf::Vector2<int>(x, y), s));
-    }
-    // закрываем файл
-    input.close();
-}
-
-// рисование параметров цвета
-void ShowBackgroundSetting() {
-    // если не раскрыта панель `Background`
-    if (!ImGui::CollapsingHeader("Background"))
-        // заканчиваем выполнение
-        return;
-
-    // Инструмент выбора цвета
-    if (ImGui::ColorEdit3("Background color", color)) {
-        // код вызывается при изменении значения
-        // задаём цвет фона
-        setColor(color);
-    }
 }
 
 // рисование задачи на невидимом окне во всё окно приложения
@@ -194,20 +77,6 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 // закрываем окно
                 window.close();
-            }
-            // если событие - это клик мышью
-            if (event.type == sf::Event::MouseButtonPressed) {
-                // если мышь не обрабатывается элементами imGui
-                if (!ImGui::GetIO().WantCaptureMouse) {
-                    // меняем координаты последней добавленной точки
-                    lastAddPosBuf[0] = event.mouseButton.x;
-                    lastAddPosBuf[1] = event.mouseButton.y;
-                    // если левая кнопка мыши
-                    if (event.mouseButton.button == sf::Mouse::Button::Left)
-                        points.emplace_back(sf::Vector2<int>(event.mouseButton.x, event.mouseButton.y), SET_1);
-                    else
-                        points.emplace_back(sf::Vector2<int>(event.mouseButton.x, event.mouseButton.y), SET_2);
-                }
             }
         }
 
