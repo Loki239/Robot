896add6c5b925e6b908aecac6d95dda0dc891743
alex <aoklyunin@gmail.com>
2022-09-30 11:20:05 +0300

    primitives ready

--- src/main.cpp
+++ src/main.cpp
@@ -103,20 +103,10 @@ static void setColor(float *pDouble) {
     bgColor.b = static_cast<sf::Uint8>(pDouble[2] * 255.f);
 }
 
-// рисование задачи на невидимом окне во всё окно приложения
-void RenderTask() {
-    // задаём левый верхний край невидимого окна
-    ImGui::SetNextWindowPos(ImVec2(0, 0));
-    // задаём правый нижний край невидимого окна
-    ImGui::SetNextWindowSize(ImVec2(WINDOW_SIZE_X, WINDOW_SIZE_Y));
-    // создаём невидимое окно
-    ImGui::Begin("Overlay", nullptr,
-                 ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
-                 ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoBackground);
+// нарисовать точку
+void renderPoint() {
     // получаем список примитивов, которые будут нарисованы
     auto pDrawList = ImGui::GetWindowDrawList();
-
-
     // рисуем точку
     pDrawList->AddCircleFilled(
             sf::Vector2i(400, 400),
@@ -124,7 +114,12 @@ void RenderTask() {
             ImColor(100, 200, 150),
             20
     );
+}
 
+// нарисовать линию
+void renderLine() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
 
     // опорные точки линии
     sf::Vector2i pointA = {450, 550};
@@ -159,11 +154,15 @@ void RenderTask() {
             ImColor(200, 100, 150),
             1.5f
     );
+}
 
-
+// рисование треугольников
+void renderTriangles() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
     // опорные точки треугольника
-    pointA = {430, 530};
-    pointB = {530, 400};
+    sf::Vector2i pointA = {430, 530};
+    sf::Vector2i pointB = {530, 400};
     sf::Vector2i pointC = {450, 450};
 
     // рисуем отрезок
@@ -204,8 +203,14 @@ void RenderTask() {
             ImColor(150, 200, 150)
     );
 
+}
+
+// рисуем круг и окружность
+void renderCircles() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
     // опорная точка окружности
-    pointA = {230, 300};
+    sf::Vector2i pointA = {230, 300};
 
     // рисуем окружность
     pDrawList->AddCircle(
@@ -223,15 +228,19 @@ void RenderTask() {
             30,
             ImColor(150, 150, 200)
     );
+}
 
-
+// рисуем параллельный прямоугольник
+void renderParallelRect() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
 
     // опорные точки параллельного четырёхугольника
-    pointA = {50, 50};
-    pointC = {200, 150};
+    sf::Vector2i pointA = {50, 50};
+    sf::Vector2i pointC = {200, 150};
 
     // вспомогательные точки параллельного четырёхугольника
-    pointB = {pointA.x, pointC.y};
+    sf::Vector2i pointB = {pointA.x, pointC.y};
     sf::Vector2i pointD = {pointC.x, pointA.y};
 
     // рисуем отрезок
@@ -265,36 +274,39 @@ void RenderTask() {
             ImColor(200, 150, 100),
             1.5f
     );
+}
 
-    // рисуем прямоугольник
+// рисуем прямоугольник
+void renderRect() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
 
     // первая вершина
-    pointA = {300, 100};
+    sf::Vector2i pointA = {300, 100};
     // вторая вершина
-    pointB = {500, 200};
+    sf::Vector2i pointB = {500, 200};
     // точка на противоположной стороне
-    sf::Vector2i pointP = {200, 200};
+    sf::Vector2i pointP = {300, 50};
 
     // рассчитываем векторы для векторного умножения
     sf::Vector2i AB = subtract(pointB, pointA);
     sf::Vector2i AP = subtract(pointP, pointA);
 
-    // определяем направление смещения
+    // определяем направление смещения(добавляем минус, т.к. ось y СК экрана смотрит вниз.
     float direction = -sign(cross(toFloat(AB), toFloat(AP)));
 
     // рассчитываем расстояние от прямой до точки
-    float dist = distance(toFloat(pointA), toFloat(pointB), toFloat(pointC));
+    float dist = distance(toFloat(pointA), toFloat(pointB), toFloat(pointP));
+
+    // получаем единичный вектор направления смещения
+    sf::Vector2f n = norm(rotated(toFloat(AB), ((float) M_PI / 2.f * direction)));
 
     // получаем вектор смещения
-    sf::Vector2i offset = toInt(mul(norm(rotated(toFloat(AB), ((float) M_PI / 2.f * direction))), dist));
-
-
-    std::cout << direction << " " << dist << " "<< length(toFloat(offset))<<std::endl;
-
+    sf::Vector2i offset = toInt(mul(n, dist));
 
     // находим координаты вторых двух вершин прямоугольника
-    pointC = sum(pointB, offset);
-    pointD = sum(pointA, offset);
+    sf::Vector2i pointC = sum(pointB, offset);
+    sf::Vector2i pointD = sum(pointA, offset);
 
     // рисуем отрезок
     pDrawList->AddLine(
@@ -341,12 +353,368 @@ void RenderTask() {
             3,
             ImColor(200, 100, 150)
     );
+
     // рисуем точку
     pDrawList->AddCircleFilled(
             pointP,
             3,
             ImColor(200, 100, 150)
     );
+}
+
+// рисуем параллелограмм
+void renderParallel() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
+
+    // первая вершина
+    sf::Vector2i pointA = {650, 150};
+    // вторая вершина
+    sf::Vector2i pointB = {600, 50};
+    // третья вершина
+    sf::Vector2i pointC = {700, 100};
+
+    // определяем вектор смещения
+    sf::Vector2i AB = subtract(pointA, pointB);
+    sf::Vector2i pointD = sum(pointC, AB);
+
+    // рисуем отрезок
+    pDrawList->AddLine(
+            pointA,
+            pointB,
+            ImColor(150, 100, 200),
+            1.5f
+    );
+    // рисуем отрезок
+    pDrawList->AddLine(
+            pointB,
+            pointC,
+            ImColor(150, 100, 200),
+            1.5f
+    );
+
+    // рисуем отрезок
+    pDrawList->AddLine(
+            pointC,
+            pointD,
+            ImColor(150, 100, 200),
+            1.5f
+    );
+
+    // рисуем отрезок
+    pDrawList->AddLine(
+            pointD,
+            pointA,
+            ImColor(150, 100, 200),
+            1.5f
+    );
+
+
+    // рисуем опорные точки
+
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointA,
+            3,
+            ImColor(200, 100, 150)
+    );
+
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointB,
+            3,
+            ImColor(200, 100, 150)
+    );
+
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointC,
+            3,
+            ImColor(200, 100, 150)
+    );
+
+}
+
+// рисуем угол
+void renderCorner() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
+
+    // вершина угла
+    sf::Vector2i pointA = {150, 400};
+    // опорные точки
+    sf::Vector2i pointB = {50, 300};
+    sf::Vector2i pointC = {80, 500};
+
+    // определяем вектор смещения
+    sf::Vector2i AB = subtract(pointB, pointA);
+    sf::Vector2i AC = subtract(pointC, pointA);
+
+
+    // получаем максимальную длину отрезка на экране, как длину диагонали экрана
+    double maxDistance = std::sqrt(WINDOW_SIZE_X * WINDOW_SIZE_X + WINDOW_SIZE_Y * WINDOW_SIZE_Y);
+
+    // получаем новые точки для рисования, которые гарантируют, что линия
+    // будет нарисована до границ экрана
+    sf::Vector2i renderPointB = sum(pointA, toInt(mul(toFloat(AB), maxDistance)));
+    sf::Vector2i renderPointC = sum(pointA, toInt(mul(toFloat(AC), maxDistance)));
+
+
+    // рисуем линию
+    pDrawList->AddLine(
+            pointA,
+            renderPointB,
+            ImColor(200, 150, 100),
+            0.5f
+    );
+
+
+    // рисуем линию
+    pDrawList->AddLine(
+            pointA,
+            renderPointC,
+            ImColor(200, 150, 100),
+            0.5f
+    );
+
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointA,
+            3,
+            ImColor(200, 100, 150)
+    );
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointB,
+            3,
+            ImColor(200, 100, 150)
+    );
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointC,
+            3,
+            ImColor(200, 100, 150)
+    );
+}
+
+// рисуем полосу
+void renderBand() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
+
+    // первая точка
+    sf::Vector2i pointA = {400, 700};
+    // вторая точка
+    sf::Vector2i pointB = {700, 500};
+    // отрезок AB
+    sf::Vector2i AB = subtract(pointB, pointA);
+    // толщина линии
+    float width = 40.f;
+
+    // вектор направления для откладывания ширины
+    sf::Vector2f wd = norm(rotated(toFloat(AB), M_PI / 2));
+    sf::Vector2i widthDirection = toInt(mul(wd, width / 2));
+
+
+    // получаем максимальную длину отрезка на экране, как длину диагонали экрана
+    double maxDistance = std::sqrt(WINDOW_SIZE_X * WINDOW_SIZE_X + WINDOW_SIZE_Y * WINDOW_SIZE_Y);
+
+    sf::Vector2i lineDirection = toInt(mul(norm(toFloat(AB)), maxDistance));
+
+    // получаем опорные точки для откладывания направления
+    sf::Vector2i basePointA = sum(pointA, widthDirection);
+    sf::Vector2i basePointB = subtract(pointA, widthDirection);
+
+    // получаем точки рисования
+    sf::Vector2i renderPointA = sum(basePointA, lineDirection);
+    sf::Vector2i renderPointB = sum(basePointB, lineDirection);
+    sf::Vector2i renderPointC = subtract(basePointB, lineDirection);
+    sf::Vector2i renderPointD = subtract(basePointA, lineDirection);
+
+    // рисуем линию
+    pDrawList->AddLine(
+            renderPointB,
+            renderPointC,
+            ImColor(150, 200, 100),
+            0.5f
+    );
+
+    // рисуем линию
+    pDrawList->AddLine(
+            renderPointD,
+            renderPointA,
+            ImColor(150, 200, 100),
+            0.5f
+    );
+
+
+
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointA,
+            3,
+            ImColor(200, 100, 150)
+    );
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointB,
+            3,
+            ImColor(200, 100, 150)
+    );
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            basePointA,
+            3,
+            ImColor(200, 100, 150)
+    );
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            basePointB,
+            3,
+            ImColor(200, 100, 150)
+    );
+}
+
+// рисуем широкий луч
+void renderWideBeam() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
+
+    // первая точка
+    sf::Vector2i pointA = {100, 600};
+    // опорные точки
+    sf::Vector2i pointB = {150, 650};
+    // отрезок AB
+    sf::Vector2i AB = subtract(pointB, pointA);
+
+
+    // получаем максимальную длину отрезка на экране, как длину диагонали экрана
+    double maxDistance = std::sqrt(WINDOW_SIZE_X * WINDOW_SIZE_X + WINDOW_SIZE_Y * WINDOW_SIZE_Y);
+
+    // создаём вектор направления для рисования условно бесконечной полосы
+    sf::Vector2i lineDirection = toInt(mul(norm(rotated(toFloat(AB), M_PI / 2)), maxDistance));
+
+    // получаем точки рисования
+    sf::Vector2i renderPointC = sum(pointA, lineDirection);
+    sf::Vector2i renderPointD = sum(pointB, lineDirection);
+
+    // рисуем линию
+    pDrawList->AddLine(
+            pointA,
+            pointB,
+            ImColor(150, 200, 100),
+            0.5f
+    );
+
+    // рисуем линию
+    pDrawList->AddLine(
+            pointA,
+            renderPointC,
+            ImColor(150, 200, 100),
+            0.5f
+    );
+
+    // рисуем линию
+    pDrawList->AddLine(
+            pointB,
+            renderPointD,
+            ImColor(150, 200, 100),
+            0.5f
+    );
+
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointA,
+            3,
+            ImColor(200, 100, 150)
+    );
+    // рисуем точку
+    pDrawList->AddCircleFilled(
+            pointB,
+            3,
+            ImColor(200, 100, 150)
+    );
+
+}
+
+
+// рисуем многоугольник
+void renderPolygon() {
+    // получаем список примитивов, которые будут нарисованы
+    auto pDrawList = ImGui::GetWindowDrawList();
+
+    // список вершин
+    ImVec2 lst[5] = {
+            ImVec2(200, 550),
+            ImVec2(250, 550),
+            ImVec2(290, 600),
+            ImVec2(190, 700),
+            ImVec2(160, 700),
+    };
+
+    // рисуем закрашенный многоугольник
+    pDrawList->AddConvexPolyFilled(
+            lst,
+            5,
+            ImColor(150, 150, 200)
+    );
+
+    // рисуем многоугольник линиями
+    pDrawList->AddPolyline(
+            lst,
+            5,
+            ImColor(200, 200, 200),
+            ImDrawListFlags_AntiAliasedLines,
+            2.5
+    );
+
+    // рисуем опорные точки
+    for (ImVec2 pos: lst) {
+        // рисуем точку
+        pDrawList->AddCircleFilled(
+                pos,
+                3,
+                ImColor(200, 100, 150)
+        );
+    }
+
+}
+
+
+// рисование задачи на невидимом окне во всё окно приложения
+void RenderTask() {
+    // задаём левый верхний край невидимого окна
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    // задаём правый нижний край невидимого окна
+    ImGui::SetNextWindowSize(ImVec2(WINDOW_SIZE_X, WINDOW_SIZE_Y));
+    // создаём невидимое окно
+    ImGui::Begin("Overlay", nullptr,
+                 ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+                 ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoInputs | ImGuiWindowFlags_NoBackground);
+
+
+    // нарисовать точку
+    renderPoint();
+    // нарисовать линию
+    renderLine();
+    // рисование треугольников
+    renderTriangles();
+    // рисуем круг и окружность
+    renderCircles();
+    // рисуем параллельный прямоугольник
+    renderParallelRect();
+    // рисуем прямоугольник
+    renderRect();
+    // рисуем параллелограмм
+    renderParallel();
+    // рисуем угол
+    renderCorner();
+    // рисуем полосу
+    renderBand();
+    // рисуем широкий луч
+    renderWideBeam();
+    // рисуем многоугольник
+    renderPolygon();
 
 
     // заканчиваем рисование окна
